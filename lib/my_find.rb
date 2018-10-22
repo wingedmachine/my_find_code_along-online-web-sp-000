570cce4757aced10bf77239dcebaa39206608fb9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 23:02:33 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.size
-    yield(collection[i])
+    if yield(collection[i])
+      return collection[1]
+    end
     i += 1
   end
 end
